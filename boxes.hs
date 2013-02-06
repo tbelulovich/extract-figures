1837b7882d77436bad8ce341c186ab4dcf3d9329
Tom Belulovich <tbelulovich@gmail.com>
2013-02-05 21:21:37 -0500

    removed shell script dependency

--- boxes.hs
+++ boxes.hs
@@ -2,6 +2,10 @@ module Main where
 
 import Control.Monad
 import Control.Applicative
+import System.IO
+import System.Process
+import System.Environment
+import Data.Foldable
 
 import qualified Data.Set as S 
 
@@ -12,6 +16,8 @@ type PointSet = S.Set Point
 readpt s = let (x,y) = span (/= ',') s
            in (read x, read (tail y))
               
+
+
 
 getboxes :: FileName -> IO [Point]
 getboxes f = do pts <- lines <$> readFile f
@@ -45,9 +51,47 @@ geometry s padding scale = let ((x,y),(z,w)) = bounds s
                                dy = scale * (w-y-2*padding)
                            in (show dx)++"x"++(show dy)++"+"++(show a)++"+"++(show b)
                         
+
+{-
 main = do pts <- lines <$> getContents
           let boxes = map readpt pts
           mapM_ (putStrLn . (\b -> geometry b 5 4)) (flood (S.fromList boxes))
-                        
+-}
+                      
+main = compute
+
+commands :: FileName -> [(String,[String])]  
+commands fn = [("convert",[fn, "text:-"]),
+               ("grep", ["magenta"]),
+               ("awk", ["'{ print substr($1,1,length($1)-1) }'"])]
+
+
+callwith hdl (c,args) 
+  = do (_,Just h,_,_) <- createProcess (proc c args){std_in=UseHandle hdl, std_out=CreatePipe}
+       return h
+
+
+
+cropfile fn out g  = do (_,_,_,p) <- createProcess (proc "convert" ["-density",
+                                                                    "288",
+                                                                    fn,
+                                                                    "-crop",
+                                                                    g,
+                                                                    out])
+                        return p
+
+
+compute = do filename <- head <$> getArgs
+             let comms = [("convert",[filename,"text:-"]),
+                          ("grep",["magenta"]),
+                          ("awk",["{ print substr($1,1,length($1)-1)}"])]           
+             h <- openFile filename ReadMode
+             pixels <- foldlM callwith h comms 
+             pts <- lines <$> hGetContents pixels
+             let boxes = map readpt pts
+             let crops = map (\b -> geometry b 5 4) (flood (S.fromList boxes))
+             zipWithM_ (\n g -> cropfile filename ("fig"++(show n)++".png") g) [0 ..] crops
+
+
 --- possible workflow: sample at 72, compute box, export at 288
 
